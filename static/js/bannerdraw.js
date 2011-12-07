4dcc88085fac5a7fc400de01a36248160668c463
kevin chou <kevinchou.c@gmail.com>
2011-12-08 02:34:53 +0800

    css js static fixd

---
+++ static/js/bannerdraw.js
@@ -0,0 +1,8 @@
+			$(document).ready(function(){
+				$("#banner3").fadeOut(500);
+				$("#banner3").slideDown("slow");
+				$(".bar3text").fadeOut(500);
+				$(".bar3text").slideDown("slow");
+
+				
+			});
